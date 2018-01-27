fa34d8cc3ba7ec6a505702eb9d1b82b13413fdb7
Ian Bovard <thebestpantsintown@gmail.com>
2018-01-26 23:07:42 -1000

    bat placeholder idle animation

--- js/game.js
+++ js/game.js
@@ -2,15 +2,22 @@
   const GAME_WIDTH = 640;
   const GAME_HEIGHT = 480;
   const GAME_CONTAINER_ID = 'game';
+  const GFX = 'gfx';
+
 
   const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, { preload, create, update });
 
-  function preload() {
+  let playerCharacter
+  let playerCharacterFloat
 
+  function preload() {
+    game.load.spritesheet(GFX, '../assets/mon3_sprite_base.png', 64, 64, 5)
   };
 
   function create() {
-
+    playerCharacter = game.add.sprite(45, 200, GFX, 0)
+    playerCharacterFloat = playerCharacter.animations.add('idleFloat')
+    playerCharacter.animations.play('idleFloat', 15, true)
   };
 
   function update() {
