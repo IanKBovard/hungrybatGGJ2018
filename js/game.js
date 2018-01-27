9c2921076afe8386a59bcf4d1374d3a5f80b7f21
Ian Bovard <thebestpantsintown@gmail.com>
2018-01-27 10:26:36 -1000

    bullet lifespan

--- js/game.js
+++ js/game.js
@@ -6,6 +6,10 @@
   const BACKGROUND_SCROLL_SPEED = 2.5;
   const PLAYER_CHARACTER_MOVE_SPEED = 4;
   const PLAYER_BULLET_SPEED = 8;
+  const SQRT_TWO = Math.sqrt(2);
+
+/*  const OBSTACLE_SPAWN_FREQ = 100;*/
+/*  const randomGenerator = new Phaser.RandomDataGenerator();*/
 
   const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, { preload, create, update });
 
@@ -23,8 +27,7 @@
   }
 
   function create() {
-    game.stage.backgroundColor = '#2d2d2d';
-    background = game.add.tileSprite(0, 0, 4000, 480, 'background');
+    background = game.add.tileSprite(0, 0, 640, 480, 'background');
 
     cursors = game.input.keyboard.createCursorKeys();
     cursors.fire = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
@@ -48,18 +51,33 @@
   function handlePlayerCharacterMovement() {
     playerCharacter.body.setZeroVelocity();
 
-    let movingH = PLAYER_CHARACTER_MOVE_SPEED;
+    let movingH = SQRT_TWO;
+    let movingV = SQRT_TWO;
+    if(cursors.up.isDown || cursors.down.isDown){
+      movingH = 1;
+    }
+    if(cursors.left.isDown || cursors.right.isDown){
+      movingV = 1;
+    }
+    switch(true){
+      case cursors.left.isDown:
+        playerCharacter.body.moveLeft(200 * movingH);
+        break;
+      case cursors.right.isDown:
+        playerCharacter.body.moveRight(200 * movingH);
+    }
     switch(true){
       case cursors.down.isDown:
-        playerCharacter.body.moveDown(200);
+        playerCharacter.body.moveDown(200 * movingV);
         break;
       case cursors.up.isDown:
-        playerCharacter.body.moveUp(200);
+        playerCharacter.body.moveUp(200 * movingV);
         break;
     }
   }
 
   function handleBulletAnimations() {
+    console.log(playerBullets)
     playerBullets.children.forEach(bullet => {
       bullet.x += PLAYER_BULLET_SPEED;
     });
@@ -67,6 +85,9 @@
 
   function handlePlayerFire() {
     playerBullets.add(game.add.sprite(playerCharacter.x + 10, playerCharacter.y - 10, 'bullets', 0));
+    playerBullets.children.forEach(bullet => {
+      bullet.lifespan = 300;
+    });
   }
 
   function handleObstacleScroll() {
@@ -76,12 +97,32 @@
   function handleBackgroundScroll() {
     background.tilePosition.x -= BACKGROUND_SCROLL_SPEED;
   }
-
+/*  function handleBulletDistance(){
+    playerBullets.children
+    .filter(bullet => bullet.x )
+  }*/
+/*  function randomlySpawnObstacles(){
+    if(randomGenerator.between(0, OBSTACLE_SPAWN_FREQ) === 0){
+      let randomY = randomGenerator.between(0, GAME_HEIGHT);
+      obstacles.add(game.add.sprite(900, randomY, GFX, 0))
+    }
+  }*/
+  function killBullet(){
+    playerBullets.children
+    .filter(bullet => {
+      console.log('HERE', bullet.lifespan)
+      if(bullet.lifespan === 0){
+        bullet.destroy();
+      }
+    })
+  }
   function update() {
     handleBackgroundScroll();
     handlePlayerCharacterMovement();
     handleObstacleScroll();
     handleBulletAnimations();
+    killBullet();
+/*    randomlySpawnObstacles();*/
   }
 
 })(window.Phaser);
