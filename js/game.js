917d463d3d201d9ca6ee1860c51c4ed88ca18bce
Ian Bovard <thebestpantsintown@gmail.com>
2018-01-26 22:32:42 -1000

    viewport boilerplate

---
+++ js/game.js
@@ -0,0 +1,20 @@
+(Phaser => {
+  const GAME_WIDTH = 640;
+  const GAME_HEIGHT = 480;
+  const GAME_CONTAINER_ID = 'game';
+
+  const game = new Phaser.Game(GAME_WIDTH, GAME_HEIGHT, Phaser.AUTO, GAME_CONTAINER_ID, { preload, create, update });
+
+  function preload() {
+
+  };
+
+  function create() {
+
+  };
+
+  function update() {
+
+  };
+
+})(window.Phaser);
